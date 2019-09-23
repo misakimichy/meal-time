4d6484256944ac4ae0a9502f0695dd47c8f7cff0
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-22 22:49:00 -0700

    Initialize the project

---
+++ MealTime.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MealTime
+{
+  static void Main()
+  {
+    Console.WriteLine("What did you eat for breakfast?");
+    string yourBreakfast = Console.ReadLine();
+    Console.WriteLine("You had " + yourBreakfast + " for breakfast.");
+    
+    Console.WriteLine("What did you eat for lunch?");
+    string yourLunch = Console.ReadLine();
+    Console.WriteLine("You had " + yourLunch + " for lunch.");
+
+    Console.WriteLine("What did you eat for dinner?");
+    string yourDinner = Console.ReadLine();
+    Console.WriteLine("You had " + yourDinner + " for dinner.");
+  }
+}
