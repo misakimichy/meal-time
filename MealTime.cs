121e61c87cebb7c038936fb73b77a5b843bc96cc
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-22 23:03:21 -0700

    Make code DRY.

--- MealTime.cs
+++ MealTime.cs
@@ -2,22 +2,16 @@ using System;
 
 class MealTime
 {
-  static void AskWhatYouAteFor (string meal)
+    static void Main()
+  {
+    AskWhatYouAteFor("breakfast");
+    AskWhatYouAteFor("lunch");
+    AskWhatYouAteFor("dinner");
+  }
+  static void AskWhatYouAteFor(string meal)
   {
     Console.WriteLine("What did you eat for " + meal + "?");
-  }
-  static void Main()
-  {
-    Console.WriteLine("What did you eat for breakfast?");
-    string yourBreakfast = Console.ReadLine();
-    Console.WriteLine("You had " + yourBreakfast + " for breakfast.");
-    
-    Console.WriteLine("What did you eat for lunch?");
-    string yourLunch = Console.ReadLine();
-    Console.WriteLine("You had " + yourLunch + " for lunch.");
-
-    Console.WriteLine("What did you eat for dinner?");
-    string yourDinner = Console.ReadLine();
-    Console.WriteLine("You had " + yourDinner + " for dinner.");
+    string yourMeal = Console.ReadLine();
+    Console.WriteLine("You had " + yourMeal + " for " + meal + ".");
   }
 }
